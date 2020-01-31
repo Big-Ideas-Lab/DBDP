97d25a16fe8af0a18aafcfd4c0126d29cb8bd71e
Chentian Jiang <chentianjiang@chentians-mbp-2.attlocal.net>
2020-01-31 08:28:30 -0600

    add initial scripts and example usage

--- DigitalBiomarkers-Resting_Heart_Rate/README.md
+++ DigitalBiomarkers-Resting_Heart_Rate/README.md
@@ -1,24 +1,35 @@
 # Resting Heart Rate
 
-**Objectives:**
+**Objectives:** Estimate the resting heart rate biomarker using:
+1. _Personal_ data (i.e. different estimates for different individuals)
+2. _Accessible_ wearable device data, specifically Fitbit data
+3. _Transparent and meaningful_ model/logic based on background literature
 
+**Organizations:** This project is part of the **Big Ideas Lab at Duke University**. We collaborated with the **DISCOVeR Lab at Stanford University**, which conducted the Strong-D study. The Strong-D Fitbit data set was used to develop, evaluate and publish this resting heart rate estimation model.
 
-**Input:**
+**Publication:**
+C. Jiang, L. Faroqi, L. Palaniappan and J. Dunn, "Estimating Personal Resting Heart Rate from Wearable Biosensor Data," _2019 IEEE EMBS International Conference on Biomedical & Health Informatics (BHI)_, Chicago, IL, USA, 2019, pp. 1-4.
+doi: 10.1109/BHI.2019.8834554
+URL: http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8834554&isnumber=8834448
 
-
-**Output:**
-
-
-**Organization:**
-
-
-**Publications:**
-
-
+Please contact the contributors of this repository if you cannot access this publication.
 
 #### Code Available Now:
 ***
+`RHR_estimation.R`: helper functions for estimating resting heart rate from Fitbit data (heart rate and steps)
+`plotting.R`: helper functions for plotting 
+`example.R`: example usage of the functions in this directory; the associated output plots are included in the `example_output` subdirectory
 
 ***
 
+#### Dependencies
+R packages:
+* `data.table`
+* `magrittr`
+* `RcppRoll`
+* `ggplot2`
+
 **Sources:**
+* [Publication: Estimating Personal Resting Heart Rate from Wearable Biosensor Data](http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8834554&isnumber=8834448)
+* [Big Ideas Lab at Duke University](http://dunn.pratt.duke.edu)
+* [DISCOVeR Lab at Stanford University](http://med.stanford.edu/discover.html)
---
+++ DigitalBiomarkers-Resting_Heart_Rate/plotting.R
@@ -0,0 +1,67 @@
+##### plotting.R
+# This script defines the R helper functions used for plotting
+
+library(ggplot2)
+
+show_linePerGroup <- function(dt, x_colStr, y_colStr, group_colStr = NULL) {
+  # returns a ggplot line plot (one line per unique value in the column with name group_colStr)
+  
+  # dt : data.table
+  # x_colStr: name (character) of variable to plot on the x-axis
+  # y_colStr: name (character) of variable to plot on the y-axis
+  # group_colStr: name (character) of variable for which we want to plot one line per unique value
+  
+  plt <- 
+    ggplot(data = dt) + 
+    geom_line(mapping = aes_string(x = x_colStr, y = y_colStr, group = group_colStr, color = group_colStr)) +
+    theme(legend.position = "none") 
+  
+  return(plt)
+}
+
+# Multiple plot function
+# source: http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
